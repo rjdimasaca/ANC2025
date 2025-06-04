fd633c3df05e10fd5ab30b57b320502837ea3764
rodmard <rjdimasaca@gmail.com>
2025-06-04 14:53:40 -0600

    progress as of june 03 2025 12:17:00PM MT -new version, built together with bashar as an exercise -this now reads directly from purchase orders' quantity billed, so it now handles even when bills are deleted, it also now handles multi bills since it is now directly sending PO BILLED QTY, not just the quantity present on the bill being submitted.

--- src/FileCabinet/SuiteScripts/ANC_UE_SKYDOC_HELPER.js/ANC_UE_VENDORBILL.js
+++ src/FileCabinet/SuiteScripts/ANC_UE_SKYDOC_HELPER.js/ANC_UE_VENDORBILL.js
@@ -48,7 +48,8 @@ define(['N/https', 'N/query', 'N/record', 'N/runtime', 'N/search'],
         {
             try
             {
-                syncBill_via_ss(scriptContext);
+                // syncBill_via_ss(scriptContext);
+                syncPoBilledQtys(scriptContext);
             }
             catch(e)
             {
@@ -56,125 +57,134 @@ define(['N/https', 'N/query', 'N/record', 'N/runtime', 'N/search'],
             }
         }
 
-        var ESB_VB_ENDPOINT = `https://esbtest.albertanewsprint.com/billstatus`
-        function syncBill_via_ss(scriptContext)
+        function syncPoBilledQtys(scriptContext)
         {
-            if(scriptContext.newRecord.type == "vendorbill")
+            //get bill id
+            //handle 3 kinds of submission
+            //submission1 - creation
+            //submission2 - modification
+            //submission3 - deletion
+
+            //the biggest difference is that the record is not accessible thru NEWRECORD for delete events
+            // since the resource is erased or ceases to exist anymore
+            // so we will utilize the OLDRECORD
+            let recObj = null;
+            if(scriptContext.type == "delete")
             {
-                var respObj = {};
-                var vb_lines = [];
-                respObj.vb_internalid = scriptContext.newRecord.id;
-                var vendorbillSearchObj = search.create({
-                    type: "vendorbill",
-                    settings:[{"name":"consolidationtype","value":"ACCTTYPE"}],
-                    filters:
-                        [
-                            ["internalid","anyof",scriptContext.newRecord.id],
-                            "AND",
-                            ["type","anyof","VendBill"],
-                            "AND",
-                            ["createdfrom.externalidstring","startswith","EAM"],
-                            "AND",
-                            ["mainline","is","F"],
-                            "AND",
-                            ["createdfrom.type","anyof","PurchOrd"],
-                            "AND",
-                            ["custcol_syncmspo_lineno","isnotempty",""]
-                        ],
-                    columns:
-                        [
-                            search.createColumn({name: "internalid", label: "bill_internalid"}),
-                            search.createColumn({name: "tranid", label: "bill_tranid"}),
-                            search.createColumn({name: "transactionnumber", label: "bill_trannum"}),
-                            search.createColumn({name: "externalid", label: "bill_externalid"}),
-                            search.createColumn({
-                                name: "internalid",
-                                join: "createdFrom",
-                                label: "po_internalid"
-                            }),
-                            search.createColumn({
-                                name: "externalid",
-                                join: "createdFrom",
-                                label: "po_externalid"
-                            }),
-                            search.createColumn({
-                                name: "tranid",
-                                join: "createdFrom",
-                                label: "po_docnum"
-                            }),
-                            search.createColumn({name: "line", label: "billline_lineid"}),
-                            search.createColumn({name: "custcol_syncmspo_lineno", label: "poline_external_lineno"}),
-                            search.createColumn({name: "quantityuom", label: "bill_qty"}),
-                            search.createColumn({name: "quantity", label: "bill_qty_inbaseunits"}),
-                            // search.createColumn({
-                            //     name: "quantityuom",
-                            //     join: "createdFrom",
-                            //     label: "poline_qty"
-                            // }),
-                            // search.createColumn({
-                            //     name: "quantity",
-                            //     join: "createdFrom",
-                            //     label: "poline_qty_inbaseunits"
-                            // })
-                        ]
+                recObj = scriptContext.oldRecord
+            }
+            else
+            {
+                recObj = scriptContext.newRecord
+            }
+
+            //algorithm:
+            //get the PO internalid from BILL that triggered the event
+
+            let recInternalId = recObj.getValue({
+                fieldId : "createdfrom"
+            });
+            //it would appear that the system supports VB from multiple sources, not 1PO:1BILL
+            //thats why system represents it as a list, not a simple field from the VB body
+            if(!recInternalId)
+            {
+                //count how many POs are associated
+                var poSublistCount = recObj.getLineCount({
+                    sublistId : "purchaseorders"
                 });
-                var searchResultCount = vendorbillSearchObj.runPaged().count;
-                log.debug("vendorbillSearchObj result count",searchResultCount);
-                vendorbillSearchObj.run().each(function(result){
-                    // .run().each has a limit of 4,000 results
-                    var resObj = {};
-
-                    for(var a = 0 ; a < vendorbillSearchObj.columns.length ; a++)
+                log.debug("poSublistCount", poSublistCount);
+                //iterate through the PO SUBLIST
+                //set up an ARRAY to collect the PO Internalids
+                var po_internalid_list = [];
+                for(var a = 0 ; a < poSublistCount; a++)
+                {
+                    var po_internalid = recObj.getSublistValue({
+                        sublistId : "purchaseorders",
+                        fieldId : "id",
+                        line : a
+                    })
+                    if(!po_internalid_list.includes(po_internalid))
                     {
-                        var col = vendorbillSearchObj.columns[a];
-                        var resVal = result.getValue(col);
-                        if(col.label == "bill_qty" || col.label == "BILL_QTY")
-                        {
-                            resVal = Math.abs(resVal);
-                        }
-                        else if(col.label == "BILL_QTY_INBASEUNITS" || col.label == "bill_qty_inbaseunits")
-                        {
-                            resVal = Math.abs(resVal);
-                        }
-                        else if(col.label == "po_externalid")
-                        {
-                            respObj[col.label] = resVal;
-                        }
-                        else if(col.label == "po_docnum")
-                        {
-                            respObj[col.label] = resVal;
-                        }
-                        resObj[col.label] = resVal;
-
+                        po_internalid_list.push(po_internalid);
                     }
 
-                    vb_lines.push(resObj);
-                    return true;
-                });
+                }
+                log.debug("po_internalid_list", po_internalid_list);
 
-                log.debug("vb_lines", vb_lines);
+                var searchResults = [];
+                if(po_internalid_list.length > 0)
+                {
+                    var purchaseorderSearchObj = search.create({
+                        type: "purchaseorder",
+                        settings:[{"name":"consolidationtype","value":"ACCTTYPE"}],
+                        filters:
+                            [
+                                ["type","anyof","PurchOrd"],
+                                "AND",
+                                ["internalid","anyof",po_internalid_list],
+                                "AND",
+                                ["externalidstring","contains","EAMPO"],
+                                "AND",
+                                ["mainline","is","F"],
+                                "AND",
+                                ["custcol_syncmspo_lineno","isnotempty",""]
+                            ],
+                        columns:
+                            [
+                                search.createColumn({name: "custcol_syncmspo_lineno", label: "SYNCMSPO Line #"}),
+                                search.createColumn({name: "quantitybilled", label: "Quantity Billed"}),
+                                search.createColumn({name: "line", label: "Line ID"}),
+                                search.createColumn({name: "externalid", label: "External ID"})
+                            ]
+                    });
+                    purchaseorderSearchObj.run().each(function(result){
+                        // .run().each has a limit of 4,000 results
+                        searchResults.push({
+                            bill_qty : result.getValue({name: "quantitybilled", label: "Quantity Billed"}),
+                            poline_external_lineno : result.getValue({name: "custcol_syncmspo_lineno", label: "SYNCMSPO Line #"}),
+                            po_externalid : result.getValue({name: "externalid", label: "External ID"}),
+                        })
+                        return true;
+                    });
+                }
+
+                log.debug("searchResults", searchResults);
+
+                var respObj = {
+                    vb_internalid : recObj.internalid
+                };
+                var vb_lines = searchResults;
                 respObj.vb_lines = vb_lines;
                 respObj.submitmode = scriptContext.type;
 
+                log.debug("respObj", respObj)
+
                 if(respObj.vb_lines && respObj.vb_lines.length > 0)
                 {
-                    log.debug("sync via https!", respObj)
+                    var ESB_VB_ENDPOINT = `https://esbdev.albertanewsprint.com/billstatus`
+                    var  requestBody = JSON.stringify(respObj);
+                    var resp = https.post({
+                        url : ESB_VB_ENDPOINT,
+                        body : requestBody
+                    });
+
+                    log.debug("resp", resp)
                 }
                 else
                 {
-                    log.debug("DO NOT sync via https!", respObj)
+                    log.debug("DO NOT sync via https! - version2 06042025", DO NOT sync via https!)
                 }
+
             }
 
-            log.debug("respObj", respObj);
-
-            var  requestBody = JSON.stringify(respObj);
-            var resp = https.post({
-                url : ESB_VB_ENDPOINT,
-                body : requestBody
-            });
-
-            log.debug("resp", resp)
+            //all of the below points are handled because we went for the search approach.
+            //load the PO record, to analyze it
+            //check if it is a relevant PO (EXTERNALID contains EAM
+            //iterate through the relevant PO LINES
+            //check if they are relevant lines (CUSTCOL_SYNCMSPO#
+            //collect relevant lines, put them in an array
+            //format or structure it
+            //call to endpoint
         }
 
         return {beforeLoad, beforeSubmit, afterSubmit}
