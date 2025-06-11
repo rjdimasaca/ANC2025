002c00fa5a2f8d161ab2ba912b42368b39732250
rodmard <rjdimasaca@gmail.com>
2025-06-11 12:53:02 -0600

    progress as of june 11 2025 1252pm - commit new RESTLet as new endpoint for receiving Lane Rates

---
+++ src/FileCabinet/SuiteScripts/RATES INQUIRY/ANC_RL_LANERATES.js
@@ -0,0 +1,213 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Restlet
+ *
+ * SB endpoint:https://1116623-sb2.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=5713&deploy=1
+ */
+define(['/SuiteScripts/ANC_lib.js', 'N/https', 'N/record', 'N/runtime', 'N/search', 'N/url'],
+    /**
+ * @param{https} https
+ * @param{record} record
+ * @param{runtime} runtime
+ * @param{search} search
+ * @param{url} url
+ */
+    (ANC_lib, https, record, runtime, search, url) => {
+        /**
+         * Defines the function that is executed when a GET request is sent to a RESTlet.
+         * @param {Object} requestParams - Parameters from HTTP request URL; parameters passed as an Object (for all supported
+         *     content types)
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+         *     Object when request Content-Type is 'application/json' or 'application/xml'
+         * @since 2015.2
+         */
+        const get = (requestParams) =>
+        {
+
+        }
+
+        /**
+         * Defines the function that is executed when a PUT request is sent to a RESTlet.
+         * @param {string | Object} requestBody - The HTTP request body; request body are passed as a string when request
+         *     Content-Type is 'text/plain' or parsed into an Object when request Content-Type is 'application/json' (in which case
+         *     the body must be a valid JSON)
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+         *     Object when request Content-Type is 'application/json' or 'application/xml'
+         * @since 2015.2
+         */
+        const put = (requestBody) => {
+
+        }
+
+
+
+        var integrationLogId = null;
+        var respMsg = {};
+        /**
+         * Defines the function that is executed when a POST request is sent to a RESTlet.
+         * @param {string | Object} requestBody - The HTTP request body; request body is passed as a string when request
+         *     Content-Type is 'text/plain' or parsed into an Object when request Content-Type is 'application/json' (in which case
+         *     the body must be a valid JSON)
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+         *     Object when request Content-Type is 'application/json' or 'application/xml'
+         * @since 2015.2
+         */
+        const post = (requestBody) =>
+        {
+            var responseBody = {};
+            try
+            {
+                log.debug("requestBody", requestBody);
+
+                integrationLogId = ANC_lib.submitIntegrationLog(integrationLogId,{request:JSON.stringify(requestBody)});
+                respMsg = {
+                    success:true,
+                    requestBody,
+                    addtl_note:"hello your integration message was received, timestamp: " + new Date().getTime()
+                }
+            }
+            catch(e)
+            {
+                log.error("ERROR in function post", e)
+                respMsg={success:false, message: "ERROR caught: " + JSON.stringify(e), requestBody};
+            }
+
+            respMsg.integrationLogId = integrationLogId;
+            respMsgStr = JSON.stringify(respMsg);
+
+            integrationLogId = ANC_lib.submitIntegrationLog(integrationLogId,{response:respMsgStr});
+            log.debug("integrationLogId", integrationLogId)
+            return respMsgStr
+        }
+
+        function getRateInquiryResponse(rawData)
+        {
+
+            // var sample = {"transportationMethod":"R","methodOfPayment":"PP","releaseCode":"R","carrierID":"1250","equipment":[{"prefix":"RC60","number":"TBOX676282"}],"originStation":{"city":"Whitecourt","state":"AB"},"destinationStation":{"city":"Lulu Island","state":"BC"},"routeInformation":[{"carrierID":"1250","code":"CN","city":"Whitecourt"}],"orders":[{"orderNumber":"096847","lineItems":[{"commodity":"2621345","qualifier":"T","measures":[{"weight":71064,"weightQualifier":"E","ladingQuantity":10,"packagingCode":"ROL"}]}]}]}
+
+
+
+
+
+            var rateInquiryResponse = {};
+            try
+            {
+                rateInquiryResponse.summary = {};
+                rateInquiryResponse.list = [];
+
+                log.debug("getRateInquiryResponse rawData before HTTP POST", rawData)
+
+                var rawResp = "";
+                try
+                {
+                    var rawResp = https.post({
+                        // url: "https://esb.albertanewsprint.com:50107/TMX",
+                        url: "https://esb.albertanewsprint.com:443/TMX",
+                        body : {
+                            equipment : rawData.equipmentName,
+                            commodity : 2621345,
+                            id : 2621345,
+                            weight : 71064,
+                            // weight : 500,
+                            controlCust : 1,
+                            effectiveDate : "3/19/2024"
+                        }
+                    });
+
+                    log.debug("getRateInquiryResponse rawResp", rawResp)
+                }
+                catch(e)
+                {
+                    log.error("ERROR in function getRateInquiryResponse", e)
+                }
+
+                //TEMP
+                rawResp = {
+                    body : [
+                        {
+                            "loadID": "string",
+                            "shipmentID": "string",
+                            "rates": [
+                                {
+                                    "carrier": "163AFC5F-1B38-4EB5-BE8F-51625D46F2E3",
+                                    "lineHaulCharge": 0.1,
+                                    "route": "ANC TEST LANE1",
+                                    "railRateAuthority": "string",
+                                    "distance": 0,
+                                    "transitTime": 111,
+                                    "equipment": rawData.equipmentName,
+                                    "currency": "string",
+                                    "accessorials": [
+                                        {
+                                            "accCharge": 0.1,
+                                            "accQual": "string"
+                                        }
+                                    ],
+                                    "fuelSurcharge": 0.1,
+                                    "carrierGroupName": "string",
+                                    "totalCost": 0.1
+                                },
+                                {
+                                    "carrier": "1B5F8CFF-5BB0-4112-834C-31964EC514F0",
+                                    "lineHaulCharge": 0.1,
+                                    "route": "ANC TEST LANE2",
+                                    "railRateAuthority": "string",
+                                    "distance": 0,
+                                    "transitTime": 222,
+                                    "equipment": rawData.equipmentName,
+                                    "currency": "string",
+                                    "accessorials": [
+                                        {
+                                            "accCharge": 0.1,
+                                            "accQual": "string"
+                                        }
+                                    ],
+                                    "fuelSurcharge": 0.1,
+                                    "carrierGroupName": "string",
+                                    "totalCost": 0.1
+                                },
+                            ],
+                            "id": "string",
+                            "errors": [
+                                {
+                                    "errorDesc": "string",
+                                    "errorCode": 0
+                                }
+                            ],
+                            "effectiveDate": "2019-08-24",
+                            "timestamp": "string"
+                        }
+                    ]
+                };
+
+                if(typeof rawResp.body == "object")
+                {
+                    rateInquiryResponse.list = rawResp.body;
+                    rateInquiryResponse.firstresult = rawResp.body[0];
+                }
+
+
+
+            }
+            catch(e)
+            {
+                log.error("ERROR in function getRateInquiryResponse", e);
+            }
+            return rateInquiryResponse;
+        }
+
+        /**
+         * Defines the function that is executed when a DELETE request is sent to a RESTlet.
+         * @param {Object} requestParams - Parameters from HTTP request URL; parameters are passed as an Object (for all supported
+         *     content types)
+         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+         *     Object when request Content-Type is 'application/json' or 'application/xml'
+         * @since 2015.2
+         */
+        const doDelete = (requestParams) => {
+
+        }
+
+        return {get, put, post, delete: doDelete}
+
+    });
